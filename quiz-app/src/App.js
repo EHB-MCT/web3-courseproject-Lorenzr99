5268a95da6bf8162727f6978cbc7a0001200853f
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-10-06 10:31:28 +0200

    feat(App): HTML & CSS + refactor: class component

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,9 +1,28 @@
 import './App.scss';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {madeChoice: false};
+    this.handleChoice = this.handleChoice.bind(this);
+  }
+
+  handleChoice(e) {
+    console.log(e.target.className);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="title">QUIZ-APP</h1>
+          <div className="profile-choice">
+            <button className="profile-ask" onClick={this.handleChoice}>Vraag stellen</button>
+            <button className="profile-answer" onClick={this.handleChoice}>Vraag beantwoorden</button>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
