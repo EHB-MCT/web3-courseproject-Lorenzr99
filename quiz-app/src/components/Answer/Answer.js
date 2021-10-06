9d731995ceba7ec1e858e1cb1db4a54c6894e1d4
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-10-06 14:33:14 +0200

    feat(Answer): HTML & CSS finished

--- quiz-app/src/components/Answer/Answer.js
+++ quiz-app/src/components/Answer/Answer.js
@@ -1,7 +1,19 @@
 import './Answer.scss';
+import { Header } from '../Header/Header';
 
 export const Answer = () => {
+    function postAnswer(e) {
+      e.preventDefault();
+    }
+
     return (
-      <div></div>
+      <div className="Answer">
+        <Header />
+        <h1 className="answer-title">"question here"</h1>
+        <form className="answer-form" onSubmit={postAnswer}>
+          <input type="text" className="answer-input" placeholder="Typ hier je antwoord..."></input>
+          <input type="submit" className="answer-submit" value="Antwoord indienen"></input>
+        </form>
+      </div>
     );
 };
