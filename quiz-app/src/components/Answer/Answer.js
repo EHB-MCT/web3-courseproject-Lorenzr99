baf3b7b26222fecaa2c57e78e4536f6001e0ab63
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-10-11 14:56:23 +0200

    feat(Answer): questions fetched to component

--- quiz-app/src/components/Answer/Answer.js
+++ quiz-app/src/components/Answer/Answer.js
@@ -1,19 +1,49 @@
 import './Answer.scss';
 import { Header } from '../Header/Header';
+import React from 'react';
 
-export const Answer = () => {
-    function postAnswer(e) {
+export class Answer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {questions: []};
+    this.getQuestions = this.getQuestions.bind(this);
+    this.postAnswer = this.postAnswer.bind(this);
+  }
+
+  componentDidMount() {
+    this.getQuestions();
+  }
+    
+  async getQuestions() {
+    await fetch('http://localhost:3001/api/questions')
+    .then(response => response.json())
+    .then(response => this.setState({questions: response.docs}));
+  }
+
+    postAnswer(e, id) {
       e.preventDefault();
+      console.log("QUESTION ID: " + id);
     }
 
-    return (
+    render() {
+      const questions = this.state.questions;
+      return (
       <div className="Answer">
         <Header />
-        <h1 className="answer-title">"question here"</h1>
-        <form className="answer-form" onSubmit={postAnswer}>
-          <input type="text" className="answer-input" placeholder="Typ hier je antwoord..."></input>
-          <input type="submit" className="answer-submit" value="Antwoord indienen"></input>
-        </form>
+        <div className="answer-title">
+          {questions.map(question => {
+            return (
+              <div className="question-div" key={question._id}>
+                <span>{question.question}</span>
+                <form className="answer-form" onSubmit={e => this.postAnswer(e, question._id)}>
+                  <input type="text" className="answer-input" placeholder="Typ hier je antwoord..."></input>
+                  <input type="submit" className="answer-submit" value="Antwoord indienen"></input>
+                </form>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    );
-};
+      );
+    }
+}
