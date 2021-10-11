cc552432707a7e750a3ec30828c01bf13054f078
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-10-11 16:41:34 +0200

    feat(Answer): handleInputChange function added

--- quiz-app/src/components/Answer/Answer.js
+++ quiz-app/src/components/Answer/Answer.js
@@ -8,6 +8,7 @@ export class Answer extends React.Component {
     this.state = {questions: []};
     this.getQuestions = this.getQuestions.bind(this);
     this.postAnswer = this.postAnswer.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   componentDidMount() {
@@ -20,9 +21,18 @@ export class Answer extends React.Component {
     .then(response => this.setState({questions: response.docs}));
   }
 
+  handleInputChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
     postAnswer(e, id) {
       e.preventDefault();
       console.log("QUESTION ID: " + id);
+      console.log("ANSWER: " + this.state[id]);
     }
 
     render() {
@@ -32,11 +42,12 @@ export class Answer extends React.Component {
         <Header />
         <div className="answer-title">
           {questions.map(question => {
+            //const idWithoutNumbers = question._id.replace(/[0-9]/g, '');
             return (
               <div className="question-div" key={question._id}>
                 <span>{question.question}</span>
                 <form className="answer-form" onSubmit={e => this.postAnswer(e, question._id)}>
-                  <input type="text" className="answer-input" placeholder="Typ hier je antwoord..."></input>
+                  <input type="text" onChange={this.handleInputChange} name={question._id} className="answer-input" placeholder="Typ hier je antwoord..."></input>
                   <input type="submit" className="answer-submit" value="Antwoord indienen"></input>
                 </form>
               </div>
