e59a398645cb7ea5b54c296ca55eeeb318ae59db
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-10-11 14:54:39 +0200

    feat(express): question API get & post routes done

--- quiz-app/server.js
+++ quiz-app/server.js
@@ -1,15 +1,57 @@
+/**
+ * MONGODB CONNECTION
+ */
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://admin:C1W1K8Y3yfE2Prdg@quiz-app.54phe.mongodb.net/quiz-app?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+/**
+ * EXPRESS SERVER
+ */
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const questionRouter = express.Router();
 
-app.get('/questions', (req,res) => {
-    res.send("ALL QUESTIONS FROM DATABASE");
-});
+app.use(cors()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.post('/question', (req,res) => {
-    res.send("QUESTION POSTED");
-});
+questionRouter.route('/questions')
+    .get((req,res) => {
+        const database = client.db("quiz-app");
+        const questions = database.collection("questions");
+        questions.find().toArray(function (err, docs) {
+            console.log("Found the following documents");
+            console.log(docs);
+            res.json({message: "ALL QUESTIONS FROM DATABASE", docs});
+        });
+    })
+    .post(async (req,res) => {
+        try {
+            const database = client.db("quiz-app");
+            const questions = database.collection("questions");
+            const result = await questions.insertOne(req.body);
 
-app.listen(process.env.PORT || port, () => {
+            console.log(`A document was inserted with the _id: ${result.insertedId}`);
+            res.json({message: "QUESTION POSTED"});
+        } catch (e) {
+            console.error(e);
+            res.json({message: "QUESTION POST FAILED"});
+        }
+    });
+
+app.use('/api', questionRouter);
+
+app.listen(process.env.PORT || port, async () => {
+    try {
+        await client.connect();
+    } catch(e) {
+        console.error('CONNECTION ERROR: ' + e);
+    }
     console.log(`Quiz-App listening at http://localhost:${port}`);
 });
