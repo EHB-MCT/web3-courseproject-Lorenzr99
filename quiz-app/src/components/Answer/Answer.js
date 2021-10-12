35c615b8d0182facb1b0ae5c51b58a8a1f157a9a
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-10-12 09:36:33 +0200

    feat(Answer): answers fetched with POST to API

--- quiz-app/src/components/Answer/Answer.js
+++ quiz-app/src/components/Answer/Answer.js
@@ -29,8 +29,29 @@ export class Answer extends React.Component {
     });
   }
 
-    postAnswer(e, id) {
+    async postAnswer(e, id, question) {
       e.preventDefault();
+      if(this.state[id]) {
+        try {
+          await fetch('http://localhost:3001/api/answers', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+              },
+              body: JSON.stringify({
+                user: sessionStorage.getItem("profile-name"),
+                question_id: id,
+                question,
+                answer: this.state[id]
+              }),
+            })
+            .then(response => response.json())
+            .then(response => console.log(response.message));
+        } catch (e) {
+          console.error(e.message);
+        }
+      }
       console.log("QUESTION ID: " + id);
       console.log("ANSWER: " + this.state[id]);
     }
@@ -46,7 +67,7 @@ export class Answer extends React.Component {
             return (
               <div className="question-div" key={question._id}>
                 <span>{question.question}</span>
-                <form className="answer-form" onSubmit={e => this.postAnswer(e, question._id)}>
+                <form className="answer-form" onSubmit={e => this.postAnswer(e, question._id, question.question)}>
                   <input type="text" onChange={this.handleInputChange} name={question._id} className="answer-input" placeholder="Typ hier je antwoord..."></input>
                   <input type="submit" className="answer-submit" value="Antwoord indienen"></input>
                 </form>
