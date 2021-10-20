52bf603603f1588c59693e076e43edc574e1f914
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-10-20 14:55:22 +0200

    feat(motion-card): useState hook + variants

--- framer-motion-app/src/components/motion-card/motion-card.js
+++ framer-motion-app/src/components/motion-card/motion-card.js
@@ -1,10 +1,28 @@
 import './motion-card.scss';
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
+import { useState } from 'react';
+
+const variants = {
+    closed: {
+        height: '150px'
+    },
+    open: {
+        height: '250px'
+    }
+};
 
 export const MotionCard = (props) => {
+    const [selected, onSelect] = useState(false);
+    const [dragged, onDrag] = useState(false);
+
     return (
         <motion.div 
             className="Motion-Card"
+            onMouseDown={() => onDrag(false)}
+            onMouseMove={() => onDrag(true)}
+            onMouseUp={() => onSelect(dragged ? selected : !selected)}
+            variants={variants}
+            animate={selected ? 'open' : 'closed'}
             drag
             //dragConstraints={{ left: -100, right: 100, top: 100, bottom: 100 }}
             dragConstraints={props.constraint}
@@ -12,6 +30,9 @@ export const MotionCard = (props) => {
             whileTap={{ scale: 0.9, cursor: 'grabbing' }}
             >
             <h1 className="card-title">{props.title}</h1>
+            <div className="card-content">
+                
+            </div>
         </motion.div>
     );
 }
