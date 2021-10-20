bff902c48f9bb19e550148282d17962012ff73c0
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-10-20 17:08:23 +0200

    feat(App): API data received in useEffect hook

--- framer-motion-app/src/App.js
+++ framer-motion-app/src/App.js
@@ -1,17 +1,39 @@
 import { motion } from 'framer-motion';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import { MotionCard } from './components/motion-card/motion-card';
+import { motionCardService } from './components/motion-card/motion-card.service'; 
 
 const App = () => {
   const constraintsRef = useRef(null);
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+      motionCardService
+        .getAllDogBreeds()
+        .then(response => {
+          if(response) {
+            console.log(response)
+            const allBreeds = response.map(({ id, name, origin, life_span, temperament, bred_for }) => ({
+              id, name, origin, life_span, temperament, bred_for,
+            }));
+            setDogs(randomizeDogs(allBreeds));
+          }
+          
+        })
+        .finally(() => console.log("EFFECT DONE: getAllDogBreeds"));         
+  }, []);
+
+  const randomizeDogs = (dogs) => {
+    return dogs.sort(() => 0.5 - Math.random());
+  }
 
   return (
     <div className="App">
       <motion.div className="App-Ref" ref={constraintsRef}>
-        <MotionCard title="DOUR" constraint={constraintsRef} />
-        <MotionCard title="PUKKELPOP" constraint={constraintsRef} />
-        <MotionCard title="RAMPAGE" constraint={constraintsRef} />
+        {dogs.slice(0,3).map(dog => {
+          return <MotionCard key={dog.id} title={dog.name} constraint={constraintsRef} />
+        })}
       </motion.div>
     </div>
   );
